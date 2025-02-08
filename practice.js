4860c457dda6ce29e74346d746c34fb4b0aa8562
ismail-dev-code <ismail.online0385678@gmail.com>
2025-02-08 08:51:08 +0600

    initialization done

---
+++ practice.js
@@ -0,0 +1,17 @@
+let age = 20;
+let salary = 20000;
+let isLeader = true;
+
+if(age>18 && isLeader==true && salary <= 30000){
+    console.log("you can enter the stadium");
+}
+else{
+    console.log("You are not able enter to the stadium");
+}
+let shopKeepers = ["abul", "babul", "hablu", "gabla", "modhu"];
+if(shopKeepers.includes("gabla")){
+    console.log("this list is valid");
+}
+else{
+    console.log("the list is not valid");
+}
